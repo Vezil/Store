8b6184c03216ceed8680a639acf9081fd06f7808
Vezil <SzymonWojaczek@wp.pl>
2019-09-10 13:35:00 +0200

    Navbar

---
+++ src/components/Cart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Cart extends Component {
+
+    render(){
+        return(
+            <div>
+                <h3>HELLO FROM CART</h3>
+            </div>
+        );
+    }
+
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export default class Navbar extends Component {
+
+    render(){
+        return(
+            <div>
+                <h3>Hello NAVBAR</h3>
+            </div>
+        );
+    }
+
+}
+
+
+
+
+
+
+
+/* COMPONENTS
+
+import React, { Component } from 'react';
+
+export default class Navbar extends Component {
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+
+}
+
+*/
