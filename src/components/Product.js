d8d999d0a74a93f0fbd8efda03768927039b9ef2
Vezil <SzymonWojaczek@wp.pl>
2019-10-01 15:25:59 +0200

    Products template

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { ProductConsumer } from '../context';
+
 
 export default class Product extends Component {
 
     render(){
+        const { id, title, img, price, inCart } = this.props.product;
         return(
-            <div>
-                <h3>HELLO ONE PRODUCT</h3>
-            </div>
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+                <div className="card">
+                    <div className="img-container p-5" onClick={console.log("CLICKED IMG CONTAINER")}>
+
+
+                    </div>
+                </div>
+            </ProductWrapper>
         );
     }
 
 }
+
+const ProductWrapper = styled.div`
+
+`
