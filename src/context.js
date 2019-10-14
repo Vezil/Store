416588e86e9d10854745d2f2fa9f405dfb5adb4f
Vezil <SzymonWojaczek@wp.pl>
2019-10-15 00:07:10 +0200

    Cart functionality

--- src/context.js
+++ src/context.js
@@ -12,7 +12,10 @@ class ProductProvider extends Component {
         detailProduct:detailProduct,
         cart: [],
         modalOpen: false,
-        modalProduct: detailProduct
+        modalProduct: detailProduct,
+        cartSubTotal:0,
+        cartTax: 0,
+        cartTotal: 0
     };
 
     componentDidMount(){
@@ -77,6 +80,32 @@ closeModal = () =>{
 
 };
 
+increment = (id) =>{
+
+    console.log("increment");
+
+};
+
+decrement = (id) =>{
+
+    console.log("decrement");
+
+};
+removeItem = (id) =>{
+
+    console.log("removeItem");
+};
+
+clearCart = () =>{
+
+    console.log("clearCart");
+};
+
+addTotals = () =>{
+
+    let subTotal = 0;
+};
+
     render(){
         return(
             <ProductContext.Provider value={{
@@ -84,7 +113,11 @@ closeModal = () =>{
                 handleDetail:this.handleDetail,
                 addToCart:this.addToCart,
                 openModal:this.openModal,
-                closeModal:this.closeModal
+                closeModal:this.closeModal,
+                increment:this.increment,
+                decrement:this.decrement,
+                removeItem:this.removeItem,
+                clearCart:this.clearCart
 
             }}>
                 { this.props.children }
