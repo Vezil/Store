d5eca2d05fa2222fbe783d9e9cdd607621ff316a
Vezil <SzymonWojaczek@wp.pl>
2021-02-15 20:06:34 +0100

    add prettier and format all files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch,Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
@@ -11,20 +11,19 @@ import Modal from './components/Modal';
 import FooterG from './components/FooterG';
 
 function App() {
-  return (
-      <React.Fragment>
-        <Navbar/>
-        <Switch>
-          <Route exact path="/" component={ ProductList }/>
-          <Route path="/details" component={ Details }/>
-          <Route path="/cart" component={ Cart }/>
-          <Route component={ Default }/>
-        </Switch>
-        <Modal/>
-        <FooterG/>
-      </React.Fragment>
-  
-  );
+    return (
+        <React.Fragment>
+            <Navbar />
+            <Switch>
+                <Route exact path="/" component={ProductList} />
+                <Route path="/details" component={Details} />
+                <Route path="/cart" component={Cart} />
+                <Route component={Default} />
+            </Switch>
+            <Modal />
+            <FooterG />
+        </React.Fragment>
+    );
 }
 
 export default App;
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import { ProductConsumer } from '../context';
-import {BrowserRouter} from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { ButtonContainer } from './Button';
 
-
 export default class Details extends Component {
-
-    render(){
-        return(
-            
-                <ProductConsumer>
-                    {(value)=>{
-                       const{id,
+    render() {
+        return (
+            <ProductConsumer>
+                {(value) => {
+                    const {
+                        id,
                         company,
                         img,
                         info,
@@ -21,55 +18,88 @@ export default class Details extends Component {
                         inCart
                     } = value.detailProduct;
 
-                       return (
-                           <div className="container py-5">
+                    return (
+                        <div className="container py-5">
+                            <div className="row">
+                                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                                    {' '}
+                                    <h1>{title}</h1>{' '}
+                                </div>
+                            </div>
 
-                           <div className="row">
-                              <div className="col-10 mx-auto text-center text-slanted text-blue my-5">  <h1>{ title }</h1>  </div>
-                           </div>
-
-                           <div className="row">
-                                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">  <img src= { img } className="img-fluid" alt="productimg"/> </div>
-                               
-                                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">  
-                                
-                                    <h2> model: { title } </h2> 
-
-                                 <h4 className="text-title text-uppercase text-muted mt-3 mb-2"> made by: <span className="text-uppercase"> { company } </span> </h4>  
-                                 <h4 className="text-blue">  <strong>  price: <span>$</span>  { price }  </strong>  </h4>
-
-                                 <p className="text-capitalize font-weight-bold mt-3 mb-0"> some info about product: </p>
-
-                                 <p className="text-muted lead"> { info } </p> <div className="mb-4"><a href='http://szymonwojaczek.pl/Yerba%20Mate/Recenzje/2/CDM.php' target="_blank">{ id === 6 ? "REVIEW" : "" }</a></div> 
-
-                                {/* buttons */}
-                                 <div>
-
-                                     <Link to='/'>
-                                        <ButtonContainer><i className="fas fa-undo"></i>&nbsp;back to products</ButtonContainer>
-                                     </Link>
-                                      <ButtonContainer cart disabled={ inCart ? true : false }   onClick={() => { 
-                                          value.addToCart(id);
-                                          value.openModal(id);
-                                            
-
-                                         }}>{ inCart ? <i className="fas fa-check"></i> : <i className="fas fa-plus"></i> }  { inCart ? "inCart" : "add to cart"}</ButtonContainer>
-                                 </div>
-                                 
+                            <div className="row">
+                                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                                    {' '}
+                                    <img
+                                        src={img}
+                                        className="img-fluid"
+                                        alt="productimg"
+                                    />{' '}
                                 </div>
 
-                           </div>
-
-                         </div>
-                         
-                       );
-                    }}
-
-                </ProductConsumer>
-                
+                                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                                    <h2> model: {title} </h2>
+                                    <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                                        {' '}
+                                        made by:{' '}
+                                        <span className="text-uppercase">
+                                            {' '}
+                                            {company}{' '}
+                                        </span>{' '}
+                                    </h4>
+                                    <h4 className="text-blue">
+                                        {' '}
+                                        <strong>
+                                            {' '}
+                                            price: <span>$</span> {price}{' '}
+                                        </strong>{' '}
+                                    </h4>
+                                    <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                                        {' '}
+                                        some info about product:{' '}
+                                    </p>
+                                    <p className="text-muted lead">
+                                        {' '}
+                                        {info}{' '}
+                                    </p>{' '}
+                                    <div className="mb-4">
+                                        <a
+                                            href="http://szymonwojaczek.pl/Yerba%20Mate/Recenzje/2/CDM.php"
+                                            target="_blank"
+                                        >
+                                            {id === 6 ? 'REVIEW' : ''}
+                                        </a>
+                                    </div>
+                                    {/* buttons */}
+                                    <div>
+                                        <Link to="/">
+                                            <ButtonContainer>
+                                                <i className="fas fa-undo"></i>
+                                                &nbsp;back to products
+                                            </ButtonContainer>
+                                        </Link>
+                                        <ButtonContainer
+                                            cart
+                                            disabled={inCart ? true : false}
+                                            onClick={() => {
+                                                value.addToCart(id);
+                                                value.openModal(id);
+                                            }}
+                                        >
+                                            {inCart ? (
+                                                <i className="fas fa-check"></i>
+                                            ) : (
+                                                <i className="fas fa-plus"></i>
+                                            )}{' '}
+                                            {inCart ? 'inCart' : 'add to cart'}
+                                        </ButtonContainer>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                }}
+            </ProductConsumer>
         );
-        
     }
-
-
 }
